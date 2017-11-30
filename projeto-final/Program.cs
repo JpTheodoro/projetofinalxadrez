96cdabc69f0f64f7ed0eb19068ae023c4590e034
Joao Pedro Theodoro <joaopedrotheodoroferreira@gmail.com>
2017-11-29 22:05:00 -0200

    Impressao do Tabuleiro

--- projeto-final/Program.cs
+++ projeto-final/Program.cs
@@ -5,10 +5,9 @@ namespace projeto_final {
     class Program {
         static void Main(string[] args) {
 
-            Posicao P;
             Tabuleiro tab = new Tabuleiro(8, 8);
+            Tela.imprimirTabuleiro(tab);
 
-            Console.WriteLine("Posicao: " + P);
             Console.ReadLine();
 
         }
---
+++ projeto-final/Tela.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using tabuleiro;
+
+namespace projeto_final {
+    class Tela { 
+
+        public static void imprimirTabuleiro(Tabuleiro tab) {
+
+         for (int i=0; i<tab; i++) {
+                for(int j=0; j<tab; j++) {
+                    if (tab.peca(i,j)==null) {
+                        Console.Write("- ");
+                    }
+                    else {
+                        Console.Write(tab.peca(i, j) + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
